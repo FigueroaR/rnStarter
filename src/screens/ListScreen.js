a43bf27df6683b9137162c8a56a3df1ade84b587
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-22 15:08:29 -0500

    add Age and show exercise

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,23 +4,23 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () => {
     const friends = [
-        {name: 'Friend #1'},
-        {name: 'Friends #2'},
-        {name: 'Friends #3'},
-        {name: 'Friends #4'},
-        {name: 'Friends #5'},
+        {name: 'Friend #1', age: '99'},
+        {name: 'Friends #2', age: '98'},
+        {name: 'Friends #3', age: '97'},
+        {name: 'Friends #4', age: '96'},
+        {name: 'Friends #5', age: '95'},
     ]
 
     return (
         <FlatList 
             //horizontal // horizontal list
-            showsHorizontalScrollIndicator={false}// hides scroll bar horizontal
+            //showsHorizontalScrollIndicator={false}// hides scroll bar horizontal
 
             //this is how we use a key
             keyExtractor={ (friend => { friend.name})}
             data={friends} 
             renderItem={ ({item}) => {
-                return <Text style={styles.textStyle}>{item.name}</Text>
+                return <Text style={styles.textStyle}>{item.name} - Age: {item.age}</Text>
             } }/>)
 }
 
