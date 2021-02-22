78c07b704445dfb50c5b074bd3989cf9361e6fff
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-22 13:56:53 -0500

    jsx overview

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -3,18 +3,21 @@ import React from 'react';
 
  // <Text> is jsx saying we want to retuen some text
  const ComponentsScreen = () => {
-     const hi = "hiii"
+     const name = "MR Named"
     return (
     <View>
-        <Text style={styles.textStyle}>Component Screen!</Text>
-        <Text>{hi}</Text>
+        <Text style={styles.textStyle}>Getting Started with React Native</Text>
+        <Text style={styles.nameStyle}>my name is {name}</Text>
     </View>)
  }
 
  // here we are going to style the component
  const styles = StyleSheet.create({
      textStyle: {
-        fontSize: 30
+        fontSize: 45
+     },
+     nameStyle: {
+         fontSize: 20
      }
  })
 
