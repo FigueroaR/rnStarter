940d062e0f6fc88ff2e7af6b3752aab8ab4f4ad0
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-22 14:55:09 -0500

    solving the Key issue

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,15 +4,17 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () => {
     const friends = [
-        {name: 'Friends #1'},
-        {name: 'Friends #2'},
-        {name: 'Friends #3'},
-        {name: 'Friends #4'},
-        {name: 'Friends #5'},
+        {name: 'Friends #1', key: '1'},
+        {name: 'Friends #2', key: '2'},
+        {name: 'Friends #3', key: '3'},
+        {name: 'Friends #4', key: '4'},
+        {name: 'Friends #5', key: '5'},
     ]
 
     return (
         <FlatList 
+        //this is how we use a key
+        keyExtractor={ (friend => { friend.name})}
             data={friends} 
             renderItem={ ({item}) => {
                 return <Text>{item.name}</Text>
