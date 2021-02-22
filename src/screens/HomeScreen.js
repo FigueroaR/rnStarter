7ccfb5d7dde2f064fa087b8cf4c3089549e719c7
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-22 16:49:17 -0500

    Button in Action

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>New APP</Text>;
+  
+  return (<View>
+    <Text style={styles.text}>New APP</Text>
+    <Button 
+      onPress={() => console.log('btn presses')}
+      title="Go to Components Demo"/>
+  </View>)
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 50
+    fontSize: 40
   }
 });
 
