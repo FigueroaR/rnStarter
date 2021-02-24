084a6bba37627d869315911be64277f68141b3cf
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-23 21:04:34 -0500

    Updating State

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, TextInput} from 'react-native'
 
 const TextScreen = () => {
+    const [name, setName] = useState('')
     return (
         <View>
-            <Text>Type something</Text>
+            <Text>What is your name?</Text>
             <TextInput // we always have to give it styling or else it looks invisible
                 style={styles.input}
                 autoCapitalize="sentences"
                 autoCorrect={false}
+                value={name}
+                onChangeText={(newValue) => setName(newValue) }
                 />
+            <Text>My name is: {name}</Text>
         </View>
     )
 }
