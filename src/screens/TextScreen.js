15fbbbee7bd94fd03ad192ad25d5e7df0b03f651
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-23 20:44:30 -0500

    showing a text input

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, TextInput} from 'react-native'
 
 const TextScreen = () => {
     return (
         <View>
-            <Text>Text Screen</Text>
+            <Text>Type something</Text>
+            <TextInput // we always have to give it styling or else it looks invisible
+                style={styles.input}/>
         </View>
     )
 }
 
-const styles= StyleSheet.create({})
+const styles= StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1
+    }
+})
 
 
 export default TextScreen;
