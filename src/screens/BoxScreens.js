fd13bfca53402654fb1757a985d5f0f5472853b9
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-23 22:33:56 -0500

    alignItem with flex

--- src/screens/BoxScreens.js
+++ src/screens/BoxScreens.js
@@ -4,20 +4,24 @@ import {Text, View, StyleSheet} from 'react-native'
 
 const BoxScreen = () => {
     return (<View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>Box Screen</Text>
+        <Text style={styles.textStyle}>Box 1</Text>
+        <Text style={styles.textStyle}>Box 2</Text>
+        <Text style={styles.textStyle}>Box 3</Text>
     </View>)
 }
 
 const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 3,
-        borderColor: 'black'
+        borderColor: 'black',
+        // alignItems: stretch is default
+        alignItems: 'center'
     },
     textStyle: {
-        borderWidth: 10,
+        borderWidth: 3,
         borderColor: 'red',
         //marginVertical: 20
-        margin: 20
+        
     }
 })
 
