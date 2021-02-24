352dc1d78a9b206bc18781d1760c24064584362a
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-23 20:50:35 -0500

    Two input props

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -6,7 +6,10 @@ const TextScreen = () => {
         <View>
             <Text>Type something</Text>
             <TextInput // we always have to give it styling or else it looks invisible
-                style={styles.input}/>
+                style={styles.input}
+                autoCapitalize="sentences"
+                autoCorrect={false}
+                />
         </View>
     )
 }
